7217cddab69988e64a8e12f44092b2b6da643a5a
GOLI VASU <golivasu322@gmail.com>
2026-04-03 16:17:05 +0530

    Webinar date updated

--- src/app/webinars/software-engineers-switch-to-ai-career/WebinarBody.jsx
+++ src/app/webinars/software-engineers-switch-to-ai-career/WebinarBody.jsx
@@ -79,7 +79,7 @@ export default function WebinarBody() {
   };
 
   const webinarId = "WB-2nd-Apr-26";
-  const webinarDate = new Date(2026, 3, 2, 20, 0, 0);
+  const webinarDate = new Date(2026, 3, 5, 20, 0, 0);
 
   useEffect(() => {
     const fetchData = async () => {
